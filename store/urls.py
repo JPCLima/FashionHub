e5c866f4bdef2198bd9115912a9e2952808e8b8c
Joao Lima <jpclima93@gmail.com>
2023-07-28 16:47:11 +0200

    Add namespace to store

--- store/urls.py
+++ store/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
-
 from .views import cart, checkout, product, productList, store
 
+# Namespacing of the app
+app_name = 'store'
+
+# Routes
 urlpatterns = [
     path('', store, name='store'),
-
     path('productList', productList, name='productList'),
     path('product/<str:pk>', product, name='product'),
-
-
     path('cart', cart, name='cart'),
     path('checkout', checkout, name='checkout'),
-
-
 ]
